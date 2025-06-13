a8e2d517cad056cbccab2280fe0c4bb3a369ef1b
thiaguin06 <alcantarat219@gamil.com>
2025-06-13 17:14:32 -0300

    algumas aulas com for, of , in e foreach

---
+++ assets/Javascript/aula3/aula.js
@@ -0,0 +1,33 @@
+/*var c = 1
+do { // ele vai executar sem ver se e verdadeiro
+    console.log(c)
+    c++
+} while (c <= 6) // while ele ve se é verdadeiro pra executar
+*/
+
+const nome = [  {
+    nome: 'joao',
+    telefone: '38998776754'
+},
+{
+    nome: 'maria',
+    telefone: '38998453213'
+},
+{
+    nome: 'pedro',
+    telefone: '21934564789'
+},
+{
+    nome: 'ana',
+    telefone: '38991593226'
+},
+{
+    nome: 'lucas',
+    telefone: '38999999999'
+}
+] 
+nome.forEach((element, index,array) => { //O método .forEach() é usado para percorrer cada item de um array e executar uma função para cada elemento.
+    //no forEach ele nao funciona com o break ent no caso e mais facil usar o for of e o for normal
+    console.log(index, element) 
+    console.log(array)
+});
--- assets/Javascript/aula3/script.js
+++ assets/Javascript/aula3/script.js
@@ -5,4 +5,5 @@ const nome = ["maria", "joao", "pedro", "jose", "marcos","maria", "joao", "pedro
 for (const i of nome) { // o for of vai percorrer o array, mais desse jeito aqui tbm funciona do mesmo jeito do outro ex: for(let i = 0; i < nome.length; i++){
     console.log(i) // of ele coloca todos os nomes no console, e o in coloca os numero no console
 }
-   
+   // pra poder pegar qualquer coisa dentreo da variaves da pra usar assim :console.log(user[nome-> esse name vai pegar qualquer um que quiser porem se coloca o maria vai pega so ele,"maria"]) -> isso é a mesma coisa do console.log(users.nome)
+// for const jek in nome -> ele vai pegar todos os nomes que estão dentro da varieavel nome
